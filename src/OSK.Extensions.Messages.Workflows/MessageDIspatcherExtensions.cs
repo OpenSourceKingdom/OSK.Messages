1fe656c9e32d1bcd8079cf7142f27ffe0def44bc
BlankDev117 <mmmasent@gmail.com>
2026-07-17 22:59:05 -0400

    Add extension for synchronous iterative dispatch for messages (OSK-11)
    Motivation
    ----
    Some applications, like game apps, can not easily run async style code. To add flexibility, we should an extension capable of handling message dispatch in a non async manner
    
    Modifications
    ----
    * Added extensions project for messages that utilizes the workflows for an operation that may or may not be ignored to run the async dispatch in an easier non-async manner over several frames
    
    Result
    ----
    An extension to transmit messages via an operation is available

---
+++ src/OSK.Extensions.Messages.Workflows/MessageDIspatcherExtensions.cs
@@ -0,0 +1,20 @@
+﻿using OSK.Messages.Abstractions;
+using OSK.Operations.Outputs.Models;
+using OSK.Operations.Workflows.Models;
+using OSK.Operations.Workflows.Tasks.Async;
+
+namespace OSK.Extensions.Messages.Workflows;
+
+public static class MessageDIspatcherExtensions
+{
+    /// <summary>
+    /// Provides access to a non-async iterative task for dispatching a message when async is not usable
+    /// </summary>
+    /// <typeparam name="TMessage">The message to send</typeparam>
+    /// <param name="dispatcher">The dispatcher to send the message</param>
+    /// <param name="message">The message to send</param>
+    /// <returns>An operation for the dispatch</returns>
+    public static ITaskOperation<Output> DispatchMessage<TMessage>(this IMessageDispatcher dispatcher, TMessage message)
+        where TMessage : IMessage
+        => dispatcher.DispatchAsync(message).ToOperation();
+}
